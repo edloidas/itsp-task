e1489d97df51007801ee94ae2d3d1aed3a9440a9
edloidas <edloidas@gmail.com>
2015-10-13 17:23:28 +0300

    :fire: Remove unused fonts. Update styles.

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -5,12 +5,16 @@ import browserSync  from 'browser-sync';
 import sourcemaps   from 'gulp-sourcemaps';
 import postcss      from 'gulp-postcss';
 import cssImport    from 'postcss-import';
+import mixins       from 'postcss-mixins';
+import simpleVars   from 'postcss-simple-vars';
 import nested       from 'postcss-nested';
+import calc         from 'postcss-calc';
 import autoprefixer from 'autoprefixer';
 import mqpacker     from 'css-mqpacker';
 import csswring     from 'csswring';
 import comments     from 'postcss-discard-comments';
 import path         from 'path';
+import handleErrors from '../util/handleErrors';
 
 const exclude = CONFIG.tasks.css.exclude.map( ( pattern ) => ( '!' + path.join( CONFIG.root.src, CONFIG.tasks.css.src, `/**/${pattern}` ) ) );
 const extensions = CONFIG.tasks.css.extensions.map( ( ext ) => path.join( CONFIG.root.src, CONFIG.tasks.css.src, `/**/*.${ext}` ) );
@@ -22,17 +26,21 @@ let paths = {
 
 let processors = [
 	cssImport,
+	mixins,
+	simpleVars,
 	nested,
+	calc,
 	autoprefixer( CONFIG.tasks.css.autoprefixer ),
 	mqpacker,
 	csswring,
 	comments
 ];
 
-gulp.task( 'css', () => {
+gulp.task( 'css', ( cb ) => {
 	return gulp.src( paths.src )
 		.pipe( sourcemaps.init() )
 		.pipe( postcss( processors ) )
+		.on( 'error', handleErrors )
 		.pipe( sourcemaps.write() )
 		.pipe( gulp.dest( paths.dest ) )
 		.pipe( browserSync.reload( { stream: true } ) );
